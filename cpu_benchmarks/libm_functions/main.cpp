247629edcc340c917dc1a05b74a223c1ff11c322
Hugo Brunie <hbrunie@lbl.gov>
2020-02-11 15:46:45 -0800

    Completing output.

--- cpu_benchmarks/libm_functions/main.cpp
+++ cpu_benchmarks/libm_functions/main.cpp
@@ -9,8 +9,9 @@
 using namespace std;
 
 
-#define EXTLOOP 10
+#define EXTLOOP 1
 #define LOOP 10000
+#define TIME 1e6
 
 #define REAL double
 using namespace std;
@@ -75,8 +76,16 @@ double standardDeviation(double arr[],
 }
 
 template <typename T>
-struct stats relativeErrorStats(double * refer, T * comp, unsigned int size){
+struct stats relativeErrorStats(double * refer, T * _comp, unsigned int size){
     struct stats relerrStats;
+    double * comp;
+    if constexpr (std::is_same<T,float>::value){
+        comp  = new double(size);
+        for(int i=0; i<size ;i++)
+            comp[i] = (double) _comp[i];
+    }else if constexpr (std::is_same<T,double>::value){
+        comp = _comp;
+    }
 
     relerrStats.m = *min_element(comp, comp+size);
     relerrStats.M = *max_element(comp, comp+size);
@@ -84,7 +93,9 @@ struct stats relativeErrorStats(double * refer, T * comp, unsigned int size){
     relerrStats.stddev = standardDeviation(comp, size);
     sort(comp, comp+size);
     relerrStats.med = comp[(unsigned int) (size/2)];
-
+    if constexpr (std::is_same<T,float>::value){
+        delete []comp;
+    }
     return relerrStats;
 }
 
@@ -107,7 +118,7 @@ REAL * generateArray(string outputfile, unsigned int prec, unsigned long size, l
 }
 
 int main(int argc, char * argv[]){
-    if(argc != 4){
+    if(argc < 4){
         cerr << "Not enough args\n SIZE (size of the array containing exp inputs)\
             \n RANGEinf RANGEsup (random generation between 2**Rinf and 2**Rsup)\
             \n Optional: input file containing randon input numbers." << endl;
@@ -115,10 +126,12 @@ int main(int argc, char * argv[]){
     }
     double * inputVal;
     double * doublePrec;
+    float  * singlePrecOutput;
     double * singlePrec2Cast;
     double * singlePrec1CastOutput;
-    float * inputValSinglePrec;
-    float * singlePrec;
+    float  * singlePrec1CastInput;
+    float  * inputValSinglePrec;
+    float  * singlePrec;
 
     unsigned int SIZE = (unsigned int)atoi(argv[1]);
     long long RANGEinf = (long long)atoll(argv[2]);
@@ -127,8 +140,10 @@ int main(int argc, char * argv[]){
     string inputfile("None");
     doublePrec = new double[SIZE];
     singlePrec = new float[SIZE];
+    singlePrecOutput = new float[SIZE];
     singlePrec2Cast = new double[SIZE];
     singlePrec1CastOutput= new double[SIZE];
+    singlePrec1CastInput= new float[SIZE];
     inputValSinglePrec = new float[SIZE];
 
     if (argc == 5)
@@ -145,20 +160,23 @@ int main(int argc, char * argv[]){
         }
     }else{//inputfile == None
         cerr << "Generating ..."<<endl;
-        inputVal = generateArray(string("numbers.dat"), 15, SIZE, RANGEinf, RANGEsup);
+        inputVal = generateArray(string("numbers-") + to_string(RANGEinf) + string("-") + to_string(RANGEsup) + string(".dat"), 15, SIZE, RANGEinf, RANGEsup);
     }
     for(int i = 0; i < SIZE; i++)
         inputValSinglePrec[i] = (float) inputVal[i];
 
     for(int k = 0; k< EXTLOOP; k++){
         auto doublePrecTime = 0;
+        auto singlePrecNoCastTime = 0;
         auto singlePrec2CastTime = 0;
         auto singlePrec1CastInputTime = 0;
         auto singlePrec1CastOutputTime = 0;
         for(int j = 0; j< LOOP; j++){
+            // Warm up
             for (int i=0;i<SIZE;i++){
                 doublePrec[i] = exp(inputVal[i]);
             }
+            // double precision
             auto t0 = Time::now();
             for (int i=0;i<SIZE;i++){
                 doublePrec[i] = exp(inputVal[i]);
@@ -166,9 +184,23 @@ int main(int argc, char * argv[]){
             auto t1 = Time::now();
             doublePrecTime += std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
 
+            // Warm up
+            for (int i=0;i<SIZE;i++){
+                singlePrecOutput[i] = expf(inputValSinglePrec[i]);
+            }
+            // single precision NO cast
+            t0 = Time::now();
+            for (int i=0;i<SIZE;i++){
+                singlePrecOutput[i] = expf(inputValSinglePrec[i]);
+            }
+            t1 = Time::now();
+            singlePrecNoCastTime += std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
+
+            // Warm up
             for (int i=0;i<SIZE;i++){
                 singlePrec2Cast[i] = (double) expf((float)inputVal[i]);
             }
+            // single precision 2 casts
             t0 = Time::now();
             for (int i=0;i<SIZE;i++){
                 singlePrec2Cast[i] = (double) expf((float)inputVal[i]);
@@ -176,13 +208,23 @@ int main(int argc, char * argv[]){
             t1 = Time::now();
             singlePrec2CastTime += std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
 
-            //t0 = Time::now();
-            //for (int i=0;i<SIZE;i++){
-            //    singlePrec1CastInput[i] = expf((float)inputVal[i]);
-            //}
-            //t1 = Time::now();
-            //singlePrec1CastInputTime += std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
+            // Warmup
+            for (int i=0;i<SIZE;i++){
+                singlePrec1CastInput[i] = expf((float)inputVal[i]);
+            }
+            // single precision 1 cast in
+            t0 = Time::now();
+            for (int i=0;i<SIZE;i++){
+                singlePrec1CastInput[i] = expf((float)inputVal[i]);
+            }
+            t1 = Time::now();
+            singlePrec1CastInputTime += std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
 
+            //Warm up
+            for (int i=0;i<SIZE;i++){
+                singlePrec1CastOutput[i] = (double) expf(inputValSinglePrec[i]);
+            }
+            // single precision 1 cast out
             t0 = Time::now();
             for (int i=0;i<SIZE;i++){
                 singlePrec1CastOutput[i] = (double) expf(inputValSinglePrec[i]);
@@ -190,16 +232,44 @@ int main(int argc, char * argv[]){
             t1 = Time::now();
             singlePrec1CastOutputTime+= std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
         }
-        struct stats relErrStats = relativeErrorStats<double>(doublePrec, singlePrec2Cast, SIZE);
-        cerr.precision(15);
-        cerr << "single Precision double casts speedup: " << (double)doublePrecTime / (double)singlePrec2CastTime
-            << " ns. RelErr: mean,med,stddev,m,M " << relErrStats.mean << "," << relErrStats.med
-            << "," << relErrStats.stddev << ","<< relErrStats.m << "," << relErrStats.M <<endl;
+        // Display of time + relative error compute
+        struct stats relErrStats2Cast = relativeErrorStats<double>(doublePrec, singlePrec2Cast, SIZE);
+        struct stats relErrStats1CastIn = relativeErrorStats<float>(doublePrec, singlePrec1CastInput, SIZE);
+        struct stats relErrStats1CastOut = relativeErrorStats<double>(doublePrec, singlePrec1CastOutput, SIZE);
+        struct stats relErrStatsNoCast = relativeErrorStats<float>(doublePrec, singlePrecOutput, SIZE);
+        cerr.precision(2);
+        cerr << "time(ns) RelErrMean med stddev min MAX" <<endl;
+        cerr << "doublePrec singlePrec2casts singlePrec1CastIn sinblePrec1castOUt singlePrecNoCast" << endl;
+        cerr << (double)doublePrecTime                                                                          / TIME     << "\t"
+             << (double)singlePrec2CastTime                                                                     / TIME     << "\t"
+             << (double)singlePrec1CastInputTime                                                                / TIME     << "\t"
+             << (double)singlePrec1CastOutputTime                                                               / TIME     << "\t"
+             << (double)singlePrecNoCastTime                                                                    / TIME     << endl;
 
-        relErrStats = relativeErrorStats<double>(doublePrec, singlePrec1CastOutput, SIZE);
-        cerr << "single Precision one cast output speedup: " << (double)doublePrecTime / (double)singlePrec1CastOutputTime
-            << " ns. RelErr: mean,med,stddev,m,M " << relErrStats.mean << "," << relErrStats.med
-            << "," << relErrStats.stddev << ","<< relErrStats.m << "," << relErrStats.M <<endl;
+        cerr << relErrStats2Cast.mean                                                                          << "\t"
+             << relErrStats1CastIn.mean                                                                     << "\t"
+             << relErrStats1CastOut.mean                                                                    << "\t"
+             << relErrStatsNoCast.mean                                                                         << endl;
+
+        cerr << relErrStats2Cast.med                                                                          << "\t"
+             << relErrStats1CastIn.med                                                                     << "\t"
+             << relErrStats1CastOut.med                                                                    << "\t"
+             << relErrStatsNoCast.med                                                                         << endl;
+
+        cerr << relErrStats2Cast.stddev                                                                          << "\t"
+             << relErrStats1CastIn.stddev                                                                     << "\t"
+             << relErrStats1CastOut.stddev                                                                    << "\t"
+             << relErrStatsNoCast.stddev                                                                         << endl;
+
+        cerr << relErrStats2Cast.m                                                                          << "\t"
+             << relErrStats1CastIn.m                                                                     << "\t"
+             << relErrStats1CastOut.m                                                                    << "\t"
+             << relErrStatsNoCast.m                                                                         << endl;
+
+        cerr << relErrStats2Cast.M                                                                          << "\t"
+             << relErrStats1CastIn.M                                                                     << "\t"
+             << relErrStats1CastOut.M                                                                    << "\t"
+             << relErrStatsNoCast.M                                                                         << endl;
 
         //std::cerr << "Expf("<<singlePrec[SIZE-1]<<") float: " << singlePrecTime/1000. << " ns " << inputVal[SIZE-1]<< " with cast cost." << std::endl;
         //std::cerr << "Expf("<<singlePrec[SIZE-1]<<") float: " << singlePrecTime/1000. << " ns " << inputValSinglePrec[SIZE-1]<< " without cast cost." << std::endl;
